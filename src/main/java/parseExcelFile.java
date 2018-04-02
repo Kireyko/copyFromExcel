eca4a46ba3a5dc660a752f1a0b3e008f15f84e46
kyreiko <kireyko@gmail.com>
2018-04-02 19:18:00 +0300

    impl_2

--- src/main/java/parseExcelFile.java
+++ src/main/java/parseExcelFile.java
@@ -3,16 +3,22 @@ import org.apache.poi.ss.usermodel.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class parseExcelFile {
     //public static final String SAMPLE_XLS_FILE_PATH = "./sample-xls-file.xls";
     //private String XLSX_FILE_PATH = "./1.xlsx";
+    private static int month;
     private Map<String, String[]> usersShiftsMap = new LinkedHashMap<>();
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm");
 
 
     public parseExcelFile(String filePath, int sheetInd, int month ) throws IOException, InvalidFormatException {
+        this.month=month;
         Workbook workbook = WorkbookFactory.create(new File(filePath));
         // Retrieving the number of sheets in the Workbook
         System.out.println("Workbook has " + workbook.getNumberOfSheets() + " Sheets : ");
@@ -24,8 +30,8 @@ public class parseExcelFile {
 
         Sheet sheet = workbook.getSheetAt(sheetInd);
         sheetParse(sheet);
-        showSheetContent();
-        convertToDate(month);
+        //showSheetContent();
+        processParsedArray();
         workbook.close();
     };
 
@@ -34,7 +40,7 @@ public class parseExcelFile {
 
         System.out.println("Parse selected sheet: "+ sheet.getSheetName());
         for (Row row: sheet) {
-            String[] rowArray = new String[32];
+            String[] rowArray = new String[31];
             for(Cell cell: row) {
                 int cellInd = cell.getColumnIndex();
                 if (cellInd==0) {
@@ -49,7 +55,7 @@ public class parseExcelFile {
     private void showSheetContent(){
         //Show/save to database
         for (Map.Entry<String, String[]> entry: usersShiftsMap.entrySet()) {
-            String key = entry.getKey();
+            String key = entry.getKey().trim();
             System.out.println();
             System.out.println(key);
 
@@ -60,20 +66,55 @@ public class parseExcelFile {
         }
     }
 
-    private void convertToDate(int month){
+    private void processParsedArray(){
         for (Map.Entry<String, String[]> entry: usersShiftsMap.entrySet()) {
-            String key = entry.getKey();
+            String key = entry.getKey().trim();
             System.out.println();
             System.out.println(key);
 
             for (int t=0; t<entry.getValue().length; t++) {
-                String val = entry.getValue()[t] ;
+                String val = entry.getValue()[t];
 
-                System.out.println(t +": "+ val);
+                if (val != null && !val.isEmpty()){
+                    System.out.println(t +": "+ val);
+                    parseCell(val.trim(), t);
+                    System.out.println();
+                }
             }
         }
 
     }
+
+    private void parseCell(String value, int position){
+
+        String[] shifts = value.split("\\/");
+        for (String shift:shifts ) {
+            //parse shift
+            String[] shiftHours = shift.split("\\-");
+            for (String strHour:shiftHours ) {
+                //System.out.println(strHour.trim());
+                convertToTime(strHour, position);
+            }
+        }
+    }
+
+    private void convertToTime(String strVal, int position){
+        String day = "2018." +String.format("%02d",month)+"." + String.format("%02d", position+1)+" ";
+
+        String strDateTime;
+        if (strVal.indexOf(":")>0) strDateTime= day + strVal;
+        else strDateTime= day + strVal + ":00";
+
+        Date date  = null;
+        try {
+            date = simpleDateFormat.parse(strDateTime);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        System.out.println(simpleDateFormat.format(date));
+
+    }
+
 
     public static void main(String[] args) throws IOException, InvalidFormatException {
         parseExcelFile parseExcelFile = new parseExcelFile("./1.xlsx", 0, 4 );
